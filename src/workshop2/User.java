a6638a01d181eefdfbea93d05b3a4c1ebb1ffa5f
mlipski00 <michal.lipski2@gmail.com>
2018-03-21 23:52:29 +0100

    commit

--- src/workshop2/User.java
+++ src/workshop2/User.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User {
 
@@ -13,6 +15,33 @@ public class User {
 	private String password;
 	private int person_group_id;
 
+	public int getId() {
+		return id;
+	}
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPerson_group_id() {
+		return person_group_id;
+	}
+
+	public void setPerson_group_id(int person_group_id) {
+		this.person_group_id = person_group_id;
+	}
+
 	public User(String username, String email, String password) {
 
 		this.username = username;
@@ -20,7 +49,7 @@ public class User {
 		this.setPassword(password);
 	}
 
-	private void setPassword(String password2) {
+	public void setPassword(String password) {
 		this.password = BCrypt.hashpw(password, BCrypt.gensalt());
 
 	}
@@ -42,6 +71,69 @@ public class User {
 			if (rs.next()) {
 				this.id = rs.getInt(1);
 			}
+		} else {
+			String sql = "UPDATE Users SET username=?, email=?, password=? where id = ?";
+			PreparedStatement preparedStatement;
+			preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setString(1, this.username);
+			preparedStatement.setString(2, this.email);
+			preparedStatement.setString(3, this.password);
+			preparedStatement.setInt(4, this.id);
+			preparedStatement.executeUpdate();
 		}
 	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password
+				+ ", person_group_id=" + person_group_id + "]\n";
+	}
+
+	static public User loadUserById(Connection conn, int id) throws SQLException {
+		String sql = "SELECT * FROM Users where id=?";
+		PreparedStatement preparedStatement;
+		preparedStatement = conn.prepareStatement(sql);
+		preparedStatement.setInt(1, id);
+		ResultSet resultSet = preparedStatement.executeQuery();
+		if (resultSet.next()) {
+			User loadedUser = new User();
+			loadedUser.id = resultSet.getInt("id");
+			loadedUser.username = resultSet.getString("username");
+			loadedUser.password = resultSet.getString("password");
+			loadedUser.email = resultSet.getString("email");
+			return loadedUser;
+		}
+		return null;
+	}
+
+	static public List<User> loadAllUsers(Connection conn) throws SQLException {
+		ArrayList<User> users = new ArrayList<User>();
+		String sql = "SELECT * FROM Users";
+		PreparedStatement preparedStatement;
+		preparedStatement = conn.prepareStatement(sql);
+		ResultSet resultSet = preparedStatement.executeQuery();
+		while (resultSet.next()) {
+			User loadedUser = new User();
+			loadedUser.id = resultSet.getInt("id");
+			loadedUser.username = resultSet.getString("username");
+			loadedUser.password = resultSet.getString("password");
+			loadedUser.email = resultSet.getString("email");
+			users.add(loadedUser);
+		}
+		//pierwotnie metoda zwracała tablice (User[])
+		//User[] uArray = new User[users.size()];
+		//uArray = users.toArray(uArray);
+		return users;
+	}
+	public void delete(Connection conn) throws SQLException {
+		if (this.id != 0) {
+		String sql = "DELETE FROM Users WHERE id= ?";
+		PreparedStatement preparedStatement;
+		preparedStatement = conn.prepareStatement(sql);
+		preparedStatement.setInt(1, this.id);
+		preparedStatement.executeUpdate();
+		this.id=0;
+		}
+		}
+
 }
